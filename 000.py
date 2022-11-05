12c133c6f5a09ab55fe224221d0068d74d140a9e
zzzzzzasd123 <112676929+zzzzzzasd123@users.noreply.github.com>
2022-11-05 23:04:40 +0900

    Add files via upload

---
+++ 000.py
@@ -0,0 +1,13 @@
+Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=============== RESTART: C:/Users/Administrator/Desktop/12341.py ===============
+문자열을 입력하시오: python
+pyon
+문자열을 입력하시오: 청소
+청소하는 중
+기호를 입력해주세요 : []
+중간에 삽입할 문자열을 입력하시오 : python
+[python]
+리스트 =  ['1', '2', '3', '4']
+리스트 숫자들의 합 :  10
